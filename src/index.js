334016f184f88032acf82a893777a087c03cf727
Gustavo Daniel Unser <gu2_unser@hotmail.com>
2021-11-14 23:26:31 -0300

    I have no idea what I'm doin

--- src/index.js
+++ src/index.js
@@ -3,14 +3,13 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
-import { Provider } from 'react-redux';
-import store from './store';
+import { StateProvider } from './store';
 
 ReactDOM.render(
     <React.StrictMode>
-        <Provider>
+        <StateProvider>
             <App />
-        </Provider>
+        </StateProvider>
     </React.StrictMode>,
     document.getElementById('root')
 );
