4b53282ed23c6938fda295a5e78ae286297a5189
Gustavo Daniel Unser <gu2_unser@hotmail.com>
2021-11-14 23:48:25 -0300

    commit fix...

--- src/index.js
+++ src/index.js
@@ -3,12 +3,7 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
-<<<<<<< HEAD
 import Provider from './store/index'
-=======
-import { Provider } from 'react-redux';
-import store from './store';
->>>>>>> parent of 334016f... I have no idea what I'm doin
 
 ReactDOM.render(
     <React.StrictMode>
